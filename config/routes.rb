431290a2c79fe5e547af40eb1a10b60368cfdeaa
Adam C. Pozidis <adchrpoz@gmail.com>
2021-01-18 21:42:20 +0100

    creation_of_show_route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # restaurant :index
   root to: "restaurants#index"
+  resources :restaurants, only: :show
 end
